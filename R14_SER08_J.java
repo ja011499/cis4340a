1210225cce197a54a6e6da631d6d5058739d6116
ja011499 <jlwissel2023@gmail.com>
2026-03-08 22:09:13 -0400

    R14_SER08_J.java

--- R14_SER08_J.java
+++ R14_SER08_J.java
@@ -1,25 +1,14 @@
-private static class CalendarAccessControlContext {
-  private static final AccessControlContext INSTANCE;
-    static {
-      RuntimePermission perm =
-          new RuntimePermission("accessClassInPackage.sun.util.calendar");
-      PermissionCollection perms = perm.newPermissionCollection();
-      perms.add(perm);
-      INSTANCE = new AccessControlContext(new ProtectionDomain[] {
-          new ProtectionDomain(null, perms)
-      });
-    }
-  }
-
-// ...
 try {
-  zi = AccessController.doPrivileged(
-      new PrivilegedExceptionAction<ZoneInfo>() {
-        public ZoneInfo run() throws Exception {
-          return (ZoneInfo) input.readObject();
-        }
-      }, CalendarAccessControlContext.INSTANCE);
-} catch (PrivilegedActionException pae) { /* ... */ }
-if (zi != null) {
-  zone = zi;
+  ZoneInfo zi = (ZoneInfo) AccessController.doPrivileged(
+    new PrivilegedExceptionAction() {
+      public Object run() throws Exception {
+        return input.readObject();
+      }
+  });
+  if (zi != null) {
+    zone = zi;
+  }
+} catch (Exception e) 
+{
+  // Handle error
 }
