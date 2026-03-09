192498185d9435a05a3fe702b531ae885b489474
ja011499 <jlwissel2023@gmail.com>
2026-03-08 22:09:34 -0400

    R17_JNI00_J.java

--- R17_JNI00_J.java
+++ R17_JNI00_J.java
@@ -1,28 +1,13 @@
-public final class NativeMethodWrapper {
+public final class NativeMethod {
 
-  // Private native method
-  private native void nativeOperation(byte[] data, int offset, int len);
+  // Public native method
+  public native void nativeOperation(byte[] data, int offset, int len);
 
-  // Wrapper method performs SecurityManager and input validation checks
+  // Wrapper method that lacks security checks and input validation
   public void doOperation(byte[] data, int offset, int len) {
-    // Permission needed to invoke native method
-    securityManagerCheck();
-
-    if (data == null) {
-      throw new NullPointerException();
-    }
-
-    // Copy mutable input
-    data = data.clone();
-
-    // Validate input
-    if ((offset < 0) || (len < 0) || (offset > (data.length - len))) {
-      throw new IllegalArgumentException();
-    }
-
     nativeOperation(data, offset, len);
   }
-
+  
   static {
     // Load native library in static initializer of class
     System.loadLibrary("NativeMethodLib"); 
