9b4d3d260413ad079ec50044576b95b5b0c24ee9
ja011499 <jlwissel2023@gmail.com>
2026-03-08 22:06:25 -0400

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,8 +2,6 @@ public void deleteFile(){
 
   File someFile = new File("someFileName.txt");
   // Do something with someFile
-  if (!someFile.delete()) {
-    // Handle failure to delete the file
-  }
+  someFile.delete();
 
 }
